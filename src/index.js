40aa7b2acf8f2dfb281b72f0e292fe2d3d70b4a2
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-11-01 10:28:34 +0630

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+ function component() {
+   const element = document.querySelector('h1');
+
+  element.classList.add('hello');
+
+   return element;
+ }
+
+ document.body.appendChild(component());
